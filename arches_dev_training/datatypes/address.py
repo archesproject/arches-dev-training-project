d076f8bd16a853cca03e997059fec8a102a44739
Rob Gaston <rgaston@fargeo.com>
2018-10-10 11:52:14 -0700

    adding module 3 start

--- arches_dev_training/datatypes/address.py
+++ arches_dev_training/datatypes/address.py
@@ -2,7 +2,7 @@ from arches.app.datatypes.base import BaseDataType
 
 details = {
     'datatype': 'address',
-    'iconclass': 'fa fa-file-code-o',
+    'iconclass': 'fa fa-location-arrow',
     'modulename': 'datatypes.py',
     'classname': 'AddressDataType',
     'defaultwidget': None,
@@ -17,27 +17,10 @@ details = {
 class AddressDataType(BaseDataType):
 
     def validate(self, value, source=None):
-        errors = []
-        message = 'datatype: address, value: {1} {2} - missing required properties. This data was not imported.'
-        try:
-            value['address']
-            value['x']
-            value['y']
-        except KeyError:
-            errors.append({
-                'type': 'ERROR',
-                'message': message.format(value, source)
-            })
-
-        return errors
+        return []
 
     def append_to_document(self, document, nodevalue, nodeid, tile):
-        document['strings'].append({
-            'string': nodevalue['address'],
-            'nodegroup_id': tile.nodegroup_id
-        })
+        return
 
     def get_search_terms(self, nodevalue, nodeid=None):
-        return [
-            nodevalue['address']
-        ]
+        return []
