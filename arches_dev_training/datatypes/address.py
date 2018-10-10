41ad4d1b2b5205e4b8f01de3f48711079b70cddb
Rob Gaston <rgaston@fargeo.com>
2018-10-10 11:41:27 -0700

    removing unused files

--- arches_dev_training/datatypes/address.py
+++ arches_dev_training/datatypes/address.py
@@ -1,15 +1,11 @@
 from arches.app.datatypes.base import BaseDataType
-from arches.app.models import models
-from arches.app.models.system_settings import settings
-
-geocoder = models.Widget.objects.get(name='geocoder')
 
 details = {
     'datatype': 'address',
     'iconclass': 'fa fa-file-code-o',
     'modulename': 'datatypes.py',
     'classname': 'AddressDataType',
-    'defaultwidget': geocoder,
+    'defaultwidget': None,
     'defaultconfig': None,
     'configcomponent': None,
     'configname': None,
@@ -22,6 +18,7 @@ class AddressDataType(BaseDataType):
 
     def validate(self, value, source=None):
         errors = []
+        message = 'datatype: address, value: {1} {2} - missing required properties. This data was not imported.'
         try:
             value['address']
             value['x']
@@ -29,20 +26,18 @@ class AddressDataType(BaseDataType):
         except KeyError:
             errors.append({
                 'type': 'ERROR',
-                'message': 'datatype: {0} value: {1} {2} - {3}. {4}'.format(
-                    self.datatype_model.datatype,
-                    value,
-                    source,
-                    'missing required properties',
-                    'This data was not imported.'
-                )
+                'message': message.format(value, source)
             })
 
         return errors
 
     def append_to_document(self, document, nodevalue, nodeid, tile):
-        document['strings'].append({'string': nodevalue['address'], 'nodegroup_id': tile.nodegroup_id})
+        document['strings'].append({
+            'string': nodevalue['address'],
+            'nodegroup_id': tile.nodegroup_id
+        })
 
     def get_search_terms(self, nodevalue, nodeid=None):
-        terms = [nodevalue['address']]
-        return terms
+        return [
+            nodevalue['address']
+        ]
